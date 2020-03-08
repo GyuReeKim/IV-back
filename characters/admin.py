7ec65861752be7cc327397dfdebe46659df03951
GyuReeKim <starkim96@naver.com>
2020-03-08 18:17:30 +0900

    register model

--- characters/admin.py
+++ characters/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Character, Position, Persona, Trait, Survivor, Hunter
 
 # Register your models here.
+admin.site.register(Character)
+admin.site.register(Position)
+admin.site.register(Persona)
+admin.site.register(Trait)
+admin.site.register(Survivor)
+admin.site.register(Hunter)
