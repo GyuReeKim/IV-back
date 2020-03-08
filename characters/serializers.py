68c269241507a3dfa359eacf8eed60250bb2519f
GyuReeKim <starkim96@naver.com>
2020-03-08 17:44:38 +0900

    Persona recursive serializer

--- characters/serializers.py
+++ characters/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Character, Position
+from .models import Character, Position, Persona
+from rest_framework_recursive.fields import RecursiveField
 
 
 class CharacterSerializer(serializers.ModelSerializer):
@@ -22,4 +23,18 @@ class PositionSerializer(serializers.ModelSerializer):
         #     'id', 'position_name', 
         #     'position_grade', 'position_role',
         # )
+        read_only_fields = ('id',)
+
+
+class PersonaSerializer(serializers.ModelSerializer):
+    persona_parent = RecursiveField(required=False, allow_null=True)
+
+    class Meta:
+        model = Persona
+        fields = (
+            'id', 'persona_sort', 
+            'persona_name', 'persona_direction', 
+            'persona_parent', 'persona_point', 
+            'persona_explanation', 'persona_opinion',
+        )
         read_only_fields = ('id',)
